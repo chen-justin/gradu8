c0e2517c9ca0e999e45eb2e46086169289c2e461
Evan McIntire <mcintire.evan@gmail.com>
2017-08-03 22:01:08 -0400

    Add all files, start of work

---
+++ main.js
@@ -0,0 +1,81 @@
+function addYear() {
+    console.log("hi")
+}
+
+new Vue({
+    el: "#add",
+    methods: {
+        test: function() {
+            console.log("hi")
+        }
+    }
+});
+
+Vue.component('display', {
+    template: `
+        <div>
+            <span v-show='!edit' @dblclick='edit=true;'>{{p_data}}</span>
+            <div class="input-field inp-custom" v-show='edit'>
+                <input :id="p_data" type="text" ref="inp"
+                        v-model=p_data
+                        v-on:blur='edit=false'
+                        @keyup.enter='edit=false'
+                        autofocus/>
+            </div>
+        </div>
+    `,
+    props: ['p_data'],
+    data: function() {
+        return {
+            edit: false
+        }
+    }
+});
+
+Vue.component('tr-d', {
+
+});
+
+ $(document).ready(function() {
+    $('select').material_select();
+  });
+
+Vue.component('semester', {
+    template: `<div class='semester col s3'>
+                <h4>{{time}}</h4>
+                <table class='striped'>
+                <tbody>
+                    <tr>
+                        <th>Class</th>
+                        <th>Credits</th>
+                    </tr>
+                    <tr v-for="course in courses">
+                        <td>
+                            {{course.course_id}}
+                        </td>
+                        <td>{{course.credits}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>`,
+    data: function() {
+        return {
+            courses: [{
+                course_id: "CMSC330",
+                credits: 3,
+                edit: false
+            }, {
+                course_id: "CMSC351",
+                credits: 3,
+                edit: false
+            }, {
+                course_id: "CMSC298",
+                credits: 1,
+                edit: false
+            }]
+        }
+    },
+    props: ['time']
+});
+
+new Vue({el:"#main"})
